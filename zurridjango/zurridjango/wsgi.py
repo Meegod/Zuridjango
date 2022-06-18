27d5942426a4160ad29017b9b2adc94fe8129a18
godfrey <janetomeka92@yahoo.com>
2022-06-18 19:12:06 +0100

    pythonzuri

--- zurridjango/zurridjango/wsgi.py
+++ zurridjango/zurridjango/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for starterproject project.
+WSGI config for zurridjango project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'starterproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zurridjango.settings')
 
 application = get_wsgi_application()
